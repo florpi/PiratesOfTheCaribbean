aa735ad395b84075895547adff08e130d62b8021
florpi <florpisoteadadejardin@gmail.com>
2019-11-03 15:12:45 +0000

    initial tree structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mapping disaster risk from aerial imagery',
+    author='Eduardo Cuesta-Lazaro and Carolina Cuesta-Lazaro',
+    license='MIT',
+)
