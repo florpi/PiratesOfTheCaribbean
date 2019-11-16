0b6ebfc4046e9fc5006a9b1acd35c3c9df8fdcc0
Edu <motyaedu@gmail.com>
2019-11-16 14:00:53 +0100

    setup.py installs requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import find_packages, setup
+import distutils.text_file
+from pathlib import Path
+from typing import List
+
+
+def parse_requirements(filename: str) -> List[str]:
+    """Return requirements from requirements file."""
+    # Ref: https://stackoverflow.com/a/42033122/
+    return distutils.text_file.TextFile(
+        filename=Path(__file__).with_name(filename)
+    ).readlines()
+
 
 setup(
-    name='pirates',
+    name="pirates",
     packages=find_packages(),
-    version='0.1.0',
-    description='Mapping disaster risk from aerial imagery',
-    author='Eduardo Cuesta-Lazaro and Carolina Cuesta-Lazaro',
-    license='MIT',
+    install_requires=parse_requirements("requirements.txt"),
+    version="0.1.0",
+    description="Mapping disaster risk from aerial imagery",
+    author="Eduardo Cuesta-Lazaro and Carolina Cuesta-Lazaro",
+    license="MIT",
 )
