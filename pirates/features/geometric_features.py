4f833ce7bf78480761840ae1e542292e9519bb3b
carol <florpisoteadadejardin@gmail.com>
2019-12-01 13:27:33 +0000

    more features for a simple xgboost based on non-image data

---
+++ pirates/features/geometric_features.py
@@ -0,0 +1,76 @@
+import glob
+import tqdm 
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import xarray
+import tqdm
+
+
+def neighbours_within_radius(gpd_df, cpt, radius):
+  """
+  :param gpd_df: Geopandas dataframe in which to search for points
+  :param cpt:    Point about which to search for neighbouring points
+  :param radius: Radius about which to search for neighbours
+  :return:       List of point indices around the central point, sorted by
+                 distance in ascending order
+  """
+  #Spatial index
+  sindex = gpd_df.sindex
+  #Bounding box of rtree search (West, South, East, North)
+  bbox = (cpt.x-radius, cpt.y-radius, cpt.x+radius, cpt.y+radius)
+  #Potential neighbours
+  good = []
+  for n in sindex.intersection(bbox):
+    dist = cpt.distance(gpd_df['geometry'][n])
+    if dist<radius:
+      good.append((dist,n))
+  #Sort list in ascending order by `dist`, then `n`
+  good.sort() 
+  #Return only the neighbour indices, sorted by distance in ascending order
+  return [x[1] for x in good]
+
+
+def compute_all_neighbours(gpd_df, radius):
+    idx_neighbors = []
+
+    for i, row in tqdm(gpd_df.iterrows(), total=gpd_df.shape[0]):
+        cpt = gpd_df.loc[i,'geometry'].centroid
+        neighbours = neighbours_within_radius(gpd_df,cpt,radius)
+        idx_neighbors.append(neighbours)
+
+
+    gpd_df[f"idx_neighbors_{radius}"] = idx_neighbors
+    gpd_df[f"num_neighbors_{radius}"]  = len(idx_neighbors)
+    gpd_df[f"mean_area_{radius}"] = gpd_df.loc[idx_neighbors,'geometry'].area.mean() 
+
+    return gpd_df
+
+def compute_geometric_features(geojsons):
+    for geojson in geojsons:
+        df = gpd.read_file(geojson)
+        df["subset"] = "train" if "train" in geojson else "test"
+        df["path"] = geojson
+        df = df.to_crs({'init': 'epsg:32618'})
+        dfs.append(df)
+    df = pd.concat(dfs, ignore_index=True)
+    df["place"] = df["path"].apply(lambda x: x.split("/")[5])
+    df["zone"] = df["path"].apply(lambda x: x.split("/")[6])
+    df["area"] = df["geometry"].area
+
+    radius = [20,50,100]
+    for r in radius:
+        df = compute_all_neighbours(df, r)
+
+    return df
+
+def train_val_split(gdf):
+    train_pkl = "drive/My Drive/pirates/data/processed/train/*.pkl"
+    val_pkl = "drive/My Drive/pirates/data/processed/validate/*.pkl"
+    
+    train_idx = pd.concat([pd.read_peakle(train_pkl_ind) for train_pkl_ind in glob.glob(train_pkl)]).element_id
+    val_idx = pd.concat([pd.read_peakle(val_pkl_ind) for val_pkl_ind in glob.glob(val_pkl)]).element_id
+
+    return gdf[gdf.element_id.isin(train_idx)].copy(), gdf[gdf.element_id.isin(val_idx)].copy()
+
+
